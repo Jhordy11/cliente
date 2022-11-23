a5ecc54dabf9d5bb11677ebc8d30ece70e7a91f6
Jhordy11 <fjhordy1@gmail.com>
2022-11-23 00:45:12 -0500

    uitlizando hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Weather from "./componentes/Weather";
 import PieDePagina from "./componentes/PieDePagina";
 import Calculadora from "./componentes/Calculadora";
 import Error404 from "./componentes/Error404";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, HashRouter } from "react-router-dom";
 import Presentation from "./componentes/Presentacion";
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
       className="flex flex-col items-center justify-between mx-auto  h-screen font-pay bg-gradient-to-t from-p1 to-p1 via-p3 bg-size-500 animate-tt-bg
       overflow-y-auto overflow-x-hidden gap-10 mm:gap-0"
     >
-      <BrowserRouter>
+      <HashRouter>
         <Navegador />
         <Routes>
           <Route index element={<Presentation />} />
@@ -27,7 +27,7 @@ function App() {
           <Route path="/calculadora" element={<Calculadora />} />
           <Route path="/*" element={<Error404 />}/>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
 
       <PieDePagina />
     </div>
